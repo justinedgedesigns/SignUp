1c9a17f71dd89f645e0eadc4cedb891b28a44de5
justinedgedesigns <49033411+justinedgedesigns@users.noreply.github.com>
2020-11-12 15:38:43 -0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,19 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <ul class="footer-nav">
+                    <li class="footer-link"><a href="#">Privacy Policy</a></li>
+                    <li class="footer-link"><a href="#">Terms &amp; Conditions</a></li>
+                    <li class="footer-link"><a href="#">FAQ</a></li>
+                    <li class="footer-link"><a href="#">Contact Us</a></li>
+                    <li class="footer-link"><a href="#">U.S.C. 2257 Record</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <p class="copyright">Copyright &copy; <a target="_blank" href="https://www.aebn.com">aebn</a> 1999-<script>document.write(new Date().getFullYear())</script>. All rights reserved.</p>
+            </div>
+        </div>
+    </div>
+    <
+</footer>
---
+++ header.php
@@ -0,0 +1,26 @@
+<header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="#aboutUs" class="logo"><img src="https://pic.aebn.net/next-theater-svg-logo-test/aebn_logo.svg" alt="AEBN Logo"></a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                        <li class="scroll-to-section"><a href="#aboutUs">About us</a></li>
+                        <li class="scroll-to-section"><a href="#content-partner">Content Partners</a></li>
+                        <li class="scroll-to-section"><a href="#traffic-partner">Traffic Partners</a></li>
+                        <li class="scroll-to-section"><a href="#pay-site-content">Pay-Site Content</a></li>
+                        <li class="scroll-to-section"><a href="#aebn-trends">AEBN Trends</a></li>
+                        <li class="scroll-to-section"><a href="https://www.aebn.net/index.cfm?fuseaction=main.LoginForm">Login</a></li>
+                    </ul>
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
